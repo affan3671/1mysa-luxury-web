7edac2550e3d1dba5f4497673cb553fab9bd9d91
Affan Ali <affanali3671@gmail.com>
2026-01-24 17:57:27 +0530

    Revert "Major Changes"
    This reverts commit bf4e0ed17119eadc034c9fd5883a6ec5ca2c21e3.

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ import OrderPage from "./pages/OrderPage";
 import ContactPage from "./pages/ContactPage";
 import NotFound from "./pages/NotFound";
 import FAQs from "./pages/FAQs";
+import sitemap from "./sitemap.xml?url";
 
 const GA_ID = "G-WDYZL10G97";
 const queryClient = new QueryClient();
@@ -51,6 +52,7 @@ function App() {
               <Route path="/party" element={<PartyPage />} />
               <Route path="/order" element={<OrderPage />} />
               <Route path="/contact" element={<ContactPage />} />
+              <Route path="/sitemap.xml" element={sitemap} />
               <Route path="*" element={<NotFound />} />
             </Routes>
           </BrowserRouter>
