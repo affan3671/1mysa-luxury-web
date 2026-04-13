dd0eeddd75f194d25c74d41a9b78700e4fa9b8b4
Affan Ali <affanali3671@gmail.com>
2026-04-13 16:50:48 +0530

    Added Sitemap

---
+++ src/pages/sitemap-menu.xml.ts
@@ -0,0 +1,25 @@
+export async function getServerSideProps({ res }: { res: any }) {
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset
+      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+            http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+
+  <url>
+    <loc>https://www.1mysacafe.com/menu</loc>
+    <lastmod>2026-04-13T10:58:35+00:00</lastmod>
+  </url>
+
+</urlset>`;
+
+  res.setHeader('Content-Type', 'application/xml');
+  res.write(sitemap);
+  res.end();
+
+  return { props: {} };
+}
+
+export default function Sitemap() {
+  return null;
+}
